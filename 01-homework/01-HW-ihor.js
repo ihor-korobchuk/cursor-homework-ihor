cf718a530dac4bb366e75faaeff3dd2bd8162c1f
ihor-korobchuk <ihor.korobchuk@gmail.com>
2020-11-17 15:30:53 +0200

    new changes to my homework

--- 01-homework/01-HW-ihor.js
+++ 01-homework/01-HW-ihor.js
@@ -1,8 +1,8 @@
 console.log("First homework");
 //console.log("I have changed my homework");
-let apple=15.678;
-let milk=123.965;
-let eggs=90.2345;
+const apple=15.678;
+const milk=123.965;
+const eggs=90.2345;
 console.log("Apple cost=", apple);
 console.log("Milk cost=",milk);
 console.log("Eggs cost=",eggs);
@@ -13,10 +13,8 @@ console.log("Minimum=", min);
 let summ = apple+eggs+milk;
 console.log("Summ=", summ);
 
+let CostOffCent=Math.floor(apple)+Math.floor(milk)+Math.floor(eggs);
+console.log("SummOff=", CostOffCent);
+let SummOffCent=round(summ,2);
+console.log("SummOffRound=", SummOffCent);
 
-apple=Math.round(apple);
-milk=Math.round(milk);
-eggs=Math.round(eggs);
-console.log("Apple cost=", apple);
-console.log("Milk cost=",milk);
-console.log("Eggs cost=",eggs);

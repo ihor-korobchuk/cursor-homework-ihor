de7b5728a7514d9c489914aec8bb1e1d1e1ac071
ihor-korobchuk <ihor.korobchuk@gmail.com>
2020-11-17 16:11:25 +0200

    new changes to my homework

--- 01-homework/01-HW-ihor.js
+++ 01-homework/01-HW-ihor.js
@@ -1,20 +1,41 @@
 console.log("First homework");
 //console.log("I have changed my homework");
-const apple=15.678;
-const milk=123.965;
-const eggs=90.2345;
-console.log("Apple cost=", apple);
-console.log("Milk cost=",milk);
-console.log("Eggs cost=",eggs);
-let max=Math.max(apple, eggs, milk);
-let min=Math.min(apple, eggs, milk);
+//Оголошуємо змінні
+const cat=15.678;
+const cow=123.965;
+const dog=90.2345;
+console.log("cat weight=", cat);
+console.log("cow weight=", cow);
+console.log("dog weight=", dog);
+//Знаходимо мах і мін
+let max=Math.max(cat, dog, cow);
+let min=Math.min(cat, dog, cow);
 console.log("Maximum=", max);
 console.log("Minimum=", min);
-let summ = apple+eggs+milk;
+//Знаходимо суму
+let summ = cat+dog+cow;
 console.log("Summ=", summ);
+//Відкидуємо копійки і складаємо цілу частину
+let CostOffCentCat=Math.floor(cat);
+let CostOffCentCow=Math.floor(cow);
+let CostOffCentDog=Math.floor(dog);
 
-let CostOffCent=Math.floor(apple)+Math.floor(milk)+Math.floor(eggs);
-console.log("SummOff=", CostOffCent);
-let SummOffCent=round(summ,2);
-console.log("SummOffRound=", SummOffCent);
+let SummOffCent=CostOffCentCat+CostOffCentCow+CostOffCentDog;
+console.log("SummOffCent=", SummOffCent);
+//Вивести суму округлену до сотень
+let SummRound100=Math.ceil(summ, 2)
+console.log(SummRound100);
+//Вивести чи є число парним
+
+
+//Вивести суиу решти при оплаті 500
+
+//Вивести середнє значення округлене до другого знаку
+
+
+//Створити змінну з випадковою знижкою Math.random
+let discont=Math.random();
+console.log("Your discount=",discont*100, "%");
+
+//
 

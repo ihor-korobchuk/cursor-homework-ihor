d3713495d379dfe45aa3e0e72d48fdb6a09895a9
ihor-korobchuk <ihor.korobchuk@gmail.com>
2020-11-17 21:53:47 +0200

    new changes to my homework

--- 01-homework/01-HW-ihor.js
+++ 01-homework/01-HW-ihor.js
@@ -1,55 +1,55 @@
 console.log("First homework");
 //console.log("I have changed my homework");
 //Оголошуємо змінні
-const cat=15.678;
-const cow=123.965;
-const dog=90.2345;
-console.log("cat =", cat);
-console.log("cow =", cow);
-console.log("dog =", dog);
+const priceCat=15.678;
+const priceCow=123.965;
+const priceDog=90.2345;
+console.log("cat =", priceCat);
+console.log("cow =", priceCow);
+console.log("dog =", priceDog);
 
 //Знаходимо мах і мін
-const max=Math.max(cat, dog, cow);
-const min=Math.min(cat, dog, cow);
-console.log("Maximum=", max);
-console.log("Minimum=", min);
+const maxPrice=Math.max(priceCat, priceDog, priceCow);
+const minPrice=Math.min(priceCat, priceDog, priceCow);
+console.log("Maximum=", maxPrice);
+console.log("Minimum=", minPrice);
 
 //Знаходимо суму
-const summ = cat+dog+cow;
-console.log("Summ=", summ);
+const priceSumm = priceCat+priceDog+priceCow;
+console.log("Summ=", priceSumm);
 
 //Відкидуємо копійки і складаємо цілу частину
-let CostOffCentCat=Math.floor(cat);
-let CostOffCentCow=Math.floor(cow);
-let CostOffCentDog=Math.floor(dog);
-let SummOffCent=CostOffCentCat+CostOffCentCow+CostOffCentDog;
-console.log("SummOffCent=", SummOffCent);
+const summOffCent=(Math.floor(priceCat)+Math.floor(priceCow)+Math.floor(priceDog));
+console.log("SummOffCent=", summOffCent);
 
 //Вивести суму округлену до сотень
-let RoundPrice=Math.round(summ / 100) * 100;
-console.log("RoundPrice=",RoundPrice);
+const roundPrice=Math.round(priceSumm / 100) * 100;
+console.log("RoundPrice=",roundPrice);
 
 
 
 //Вивести чи є число парним
-let CatPart=CostOffCentCat%2===0;
-let CowPart=CostOffCentCow%2===0;
-let DogPart=CostOffCentDog%2===0;
-console.log("cat cost pair?", CatPart);
-console.log("cow cost pair?", CowPart);
-console.log("dog cost pair?", DogPart);
+const priceSummPairOrNot=priceSumm%2===0;
+console.log("dog cost pair?", priceSummPairOrNo);
+
+//const catPart=costOffCentCat%2===0;
+//const cowPart=costOffCentCow%2===0;
+//const dogPart=costOffCentDog%2===0;
+//console.log("cat cost pair?", catPart);
+//console.log("cow cost pair?", cowPart);
+//console.log("dog cost pair?", dogPart);
 
 //Вивести суму решти при оплаті 500
-let cash=500;
-let change=((cash-summ).toFixed(2));
+const cash=500;
+const change=((cash-priceSumm).toFixed(2));
 console.log("Your change=", change);
 
 //Вивести середнє значення округлене до другого знаку
-const average=((summ/3).toFixed(2));
+const average=((priceSumm/3).toFixed(2));
 console.log("Average =", average)
 
 //Створити змінну з випадковою знижкою Math.random
-let discont=Math.random()*100;
+const discont=Math.random()*100;
 discont=Math.floor(discont);
 console.log("Your discount=",discont, "%");
 

ecc6dd1b2e5fe9881af1f181deb1a7cb1f4e363d
ihor-korobchuk <ihor.korobchuk@gmail.com>
2020-11-17 14:40:02 +0200

    new changes to my homework

--- 01-homework/01-HW-ihor.js
+++ 01-homework/01-HW-ihor.js
@@ -1,2 +1,8 @@
 console.log("First homework");
 //console.log("I have changed my homework");
+let apple=15.678;
+let milk=123.965;
+let eggs=90.2345;
+console.log("apple");
+console.log("milk");
+console.log("eggs");

6731df54bf669a84c1911e049bb86a8ffce50e18
ihor <ihor.korobchuk@gmail.com>
2020-12-06 18:27:48 +0200

    first case

--- 05-homework/05-HW-ihor.js
+++ 05-homework/05-HW-ihor.js
@@ -1,9 +1,14 @@
 console.log('Homework05');
 function getRandomArray(length, min, max) {
-const array = new Array(length);
-console.log(array);
-const  random = (min, max) => Math.random()*(max-min)+min;
-   array.map(item => random() );
+    let array = new Array(length).fill(0);
+    const random = (min, max) => Math.round(Math.random()*(max-min)+min);
+    const result = array.map(item =>random(min, max)
+    )
+      return result;
+
 }
-const randomArray = getRandomArray(5,6,7);
-console.log(randomArray);
+
+const rndm = (l, a,b) => new Array(l).fill(0).map(_ => Math.round(Math.random()*(b-a)+a));
+const randomArray = getRandomArray(5,6,10);
+console.log('rndm', rndm(5,6,10));
+console.log('Ihor', randomArray);
